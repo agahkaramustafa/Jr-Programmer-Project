68528acf7a6313597fb6acd9df4568674f26e1de
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2022-10-18 18:57:48 +0300

    Data persistence between sessions

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -22,6 +22,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -31,6 +32,8 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
+
         if (UnityEditor.EditorApplication.isPlaying)
         {
             UnityEditor.EditorApplication.ExitPlaymode();
@@ -41,4 +44,15 @@ public class MenuUIHandler : MonoBehaviour
             Application.Quit();
         }
     }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }    
 }
